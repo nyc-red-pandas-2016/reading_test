8ccb3a17b6069ee8e38e0c4bd0529738b7a04cdd
Richie Ito <ricito4@gmail.com>
2016-07-18 01:14:37 -0400

    get enumerable methods to work

--- enumerable.rb
+++ enumerable.rb
@@ -21,14 +21,17 @@ end
 
 def func_any(hash)
     # Check and return if any key object within the hash is of the type Integer
+    hash.any? {|key, value| value.is_a?(Integer)}
 end
 
 def func_all(hash)
     # Check and return if all the values within the hash are Integers and are < 10
+    hash.all? {|key, value| value.is_a?(Integer) && value < 10 }
 end
 
 def func_none(hash)
     # Check and return if none of the values within the hash are nil
+    hash.none? {|key, value| value== nil} 
 end
 
 def func_find(hash)
@@ -36,4 +39,25 @@ def func_find(hash)
     # [key, value] pair where the key and value are both Integers and the value is < 20
     # or [key, value] pair where the key is a String and the value is a String starting
     # with the character `a`
+    hash.find do |key, value|
+      (key.is_a?(Integer) && value.is_a?(Integer) && value < 20) || (value.is_a?(String) && value.match(/^[a]/))
+    end
 end
+
+hash_1 = {"1" => 1, "10" => 10, "string" => "string"}
+hash_2 = {"1" => 1, 9 => 9}
+hash_3= {"hello"=> nil}
+hash_4= {100 => 123, "hii" => "almost"}
+
+p func_any(hash_1)
+
+p func_all(hash_1)
+p func_all(hash_2)
+
+p func_none(hash_1)
+p func_none(hash_3) 
+
+p func_find(hash_1)
+p func_find(hash_2)
+p func_find(hash_3)
+p func_find(hash_4)
