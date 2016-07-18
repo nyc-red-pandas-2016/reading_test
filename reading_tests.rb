c7b65da86bdfb2767c7063bcf79af38a049d9569
mattk5 <mattkubota5@gmail.com>
2016-07-17 22:46:31 -0400

    worked on recursion methods.

--- reading_tests.rb
+++ reading_tests.rb
@@ -1,16 +1,16 @@
+require 'pry'
+
 def word_count(string)
   string.split.length
 end
 
 def average_word_length(string)
-    # string.split(' ').join('').length.to_f / word_count(string) if string != ''
-  if string == ""
-    return nil
-  else
-    string.split(" ") {|word| word.length.to_f / word_count(string)}
-  end
-  
+  return nil if string == ""
+    word_length = 0.0
+    string.split(" ").each {|word| word_length += word.length.to_f }
+    word_length / word_count(string)
 end
+
 
 def palindrome?(string)
   return true if string.reverse == string
@@ -18,16 +18,14 @@ def palindrome?(string)
 end
 
 def vowels(str)
-  # vowel_array = []
-  # str.split(" ").each do |word|
-  #   # if word.include?(/\D+/) == true
-  #     vowel_array << word.to_s
-  #   end
-  # end
+  vowel_array = []
+  str.split(" ").map {|word| vowel_array << word.match(/[aeiou]/)}
+    # if word.include?(/\D+/) == true
+      # vowel_array << word.to_s
 end
 
 def start_with_c?(str)
-  # str.split.map{|word| word.include?("c")}.join(' ')
+  return false if str.split.map{|word| !word.start_with?("c")}.join(' ')
 end
 
 def titlecase(str)
--- recursion.rb
+++ recursion.rb
@@ -1,5 +1,35 @@
 # Define a recursive function that returns true if a string is a palindrome and false otherwise.
+def is_palindrome?(string)
+	if string.length == 1
+		true
+	elsif string[0] != string[-1] 
+		false
+	else
+	is_palindrome?(string[1..-2])
+	end
+end
+
+p is_palindrome?('broken')
+p is_palindrome?('level')
+
+
 
 
 # Define a recursive function that flattens an array. The method should convert [[1, 2], [3, 4]] to [1, 2, 3, 4] and [[1, [8, 9]], [3, 4]] to [1, 8, 9, 3, 4].
+
+def the_flattener(array, final_array = [])
+	array.each do |idx|
+		if idx.class != Array 
+			final_array << idx
+		else
+			the_flattener(idx, final_array)
+		end
+	end
+	final_array
+end
+
+array = [[1,2], [3,[4, 5]], [6,7]]
+
+p the_flattener(array)
+
 
